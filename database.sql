7be8d61441f6adc4d58dd88cdfd4bfde175a6083
Johnny Goth <Jgoth23@gmail.com>
2021-02-18 13:53:56 -0600

    started app

--- database.sql
+++ database.sql
@@ -3,14 +3,14 @@
 
 -- Data base name "fs-react-shopping"
 
--- CREATE TABLE shopping
--- (id SERIAL PRIMARY KEY,
--- name VARCHAR (80) NOT NULL,
--- quanity DECIMAL (4,2) NOT NULL,
--- unit VARCHAR (20) NOT NULL,
--- purchased BOOLEAN DEFAULT FALSE);
+CREATE TABLE shopping
+(id SERIAL PRIMARY KEY,
+name VARCHAR (80) NOT NULL,
+quanity DECIMAL (4,2) NOT NULL,
+unit VARCHAR (20) NOT NULL,
+purchased BOOLEAN DEFAULT FALSE);
 
--- INSERT INTO "shopping" (name, quanity, unit, purchased)
--- VALUES ('Banana', 2, 'Bushel', TRUE),
--- ('Apples', 3, 'Individual', FALSE),
--- ('Carrots', 4, 'Bunch', FALSE);
+INSERT INTO "shopping" (name, quanity, unit, purchased)
+VALUES ('Banana', 2, 'Bushel', TRUE),
+('Apples', 3, 'Individual', FALSE),
+('Carrots', 4, 'Bunch', FALSE);
